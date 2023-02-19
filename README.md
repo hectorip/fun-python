90889ed4fc52d9c6840ba4fdad6cd5779458ea0a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-02-19 12:12:53 -0600

    Ejemplos de count

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Empezaremos con `itertools`, un paquete de la biblioteca estándar que permite e
 Este módulo implementa un conjunto de bloques de construcción que sirven individualmente o combinados para trabajar y crear iteradores eficientes, inspirado en APL, Haskell y SML.
 
 ## Functools
+
+??
+
+## Operator
+
+El módulo `operator` permite trabajar con los operadores que usamos como símbolos como la suma, resta, multiplicación, división, etc. como si fueran funciones. Esto es muy útil para usarlos en combinación con los iteradores.
--- itertools/iteradores_infinitos/count.py
+++ itertools/iteradores_infinitos/count.py
@@ -1,10 +1,24 @@
 
-
+from itertools import count
+import time
 """
 ¿Cómo harías una secuencia infinita de números en Python?
 Una forma es hacer un while con un contador, pero hay una forma más elegante:
 
 itertools.count(start=0, step=1)
-Es parecido a range, pero no tiene límite superior, además NO ES UNA LISTA, es un iterador.
+Es parecido a range, pero no tiene límite superior, además NO ES UNA LISTA, es un iterador,
+por lo que no ocupa mucha memoria.
 """
 
+# Esto correrá infinitamente
+counter = count()
+while True:
+    print(next(counter))
+    # Esperamos un segundo para no ver una lista de números corriendo como locos
+    time.sleep(1)
+
+# Pero el for lo hace automáticamente por nosotros
+
+for i in count():  # For llama automáticamente a next()
+    print(i)
+    time.sleep(1)
