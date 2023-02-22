ebd3817e74e0f95d8ec78e2fc26d4defed9b8a26
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-02-21 23:22:00 -0600

    Ejemplo de Repeat

---
+++ itertools/iteradores_infinitos/repeat.py
@@ -0,0 +1,8 @@
+from itertools import repeat
+# repeat repite un elemento el número de veces que le digas,
+# pero por defecto es infinito
+
+for i in repeat("Hola", 4):
+    print(i)
+
+# Repeat es útil porque no necesitas crear una lista grandísima con el mismo elemento
