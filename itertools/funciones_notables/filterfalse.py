57a32cacd5f07609303549ddb1a1eefcb2b08301
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-02-23 23:04:35 -0600

    Empezando con filterfalse

---
+++ itertools/funciones_notables/filterfalse.py
@@ -0,0 +1,8 @@
+from itertools import filterfalse
+
+# filterfalse es como un filto invertido, sólamente deja
+# los elementos que no cumplan la condición
+
+nombres = ["Juan", "Pedro", "Luis", "Ana", "María", "Lorena"]
+
+# ...
