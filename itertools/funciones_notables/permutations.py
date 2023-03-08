2deef0a1fa2e041ae0a64ea2ad42b9445de47930
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-03-08 14:04:29 -0600

    WIP permutations

--- itertools/funciones_notables/permutations.py
+++ itertools/funciones_notables/permutations.py
@@ -1,5 +1,18 @@
 """
-permutations
+Permutations
+
 Permutaciones de un iterable, de tamaño definido o del iterable completo
 
 """
+from itertools import permutations
+
+elementos = [1, 2, 3, 4, 5]
+
+# Permutaciones de tamaño 2
+
+permutaciones = permutations(elementos, 2)
+
+for p in permutaciones:
+    print(p)
+
+# Esta función es muy útil porque las permutaciones pueden crecer muy rápido.
