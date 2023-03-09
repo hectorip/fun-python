b272859a55fbb9be83fa5acc10d56d85f4a6e286
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-03-08 23:56:36 -0600

    Accumulate

--- itertools/funciones_notables/accumulate.py
+++ itertools/funciones_notables/accumulate.py
@@ -1,4 +1,18 @@
-"""Accumulate
-
-    Devuelve un iterador que va acumulando todos los valores anteriores de la secuencia.
 """
+Accumulate
+
+Devuelve un iterador que va acumulando todos los valores anteriores de la secuencia,
+muy parecido a reduce, pero haciéndolo de manera progresiva
+"""
+from itertools import accumulate
+
+valores = [1, 2, 3, 4, 5]
+
+for i in accumulate(valores):
+    print(i)
+
+# También puede recibir una función de acumulación
+
+for i in accumulate(valores, lambda x, y: x * y):
+    print(i)
+
