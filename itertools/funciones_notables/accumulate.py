b8243d35f0b3ceb262939cdc541f4ac25412fe04
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-05-13 23:41:46 -0600

    WIP

--- itertools/funciones_notables/accumulate.py
+++ itertools/funciones_notables/accumulate.py
@@ -1,4 +1,19 @@
 """Accumulate
 
     Devuelve un iterador que va acumulando todos los valores anteriores de la secuencia.
+    Por defecto, la función de acumulación es la suma, pero se puede pasar una función de acumulación.
 """
+
+from itertools import accumulate
+
+acumulados = accumulate(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
+
+for i in acumulados:
+    print(i)
+
+# También acepta una función de acumulación
+
+factoriales = accumulate(range(1, 1000), lambda x, y: x * y)
+
+for i in factoriales:
+    print(i)
